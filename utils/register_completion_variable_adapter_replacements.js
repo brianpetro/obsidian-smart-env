ee09ef88d8ec11f4b54c59738f151914d9d4ad56
Brian Joseph Petro <brian@smartconnections.app>
2025-07-30 12:07:13 -0400

    fix: add example variable strings to completion variable adapter registrations

--- utils/register_completion_variable_adapter_replacements.js
+++ utils/register_completion_variable_adapter_replacements.js
@@ -7,18 +7,22 @@ import { replace_vault_tags_var } from './replace_vault_tags_var.js';
 export function register_completion_variable_adapter_replacements(variable_adapter_class) {
   variable_adapter_class.register(
     txt => /{{\s*folder_tree\s*}}/i.test(txt),
-    replace_folder_tree_var
+    replace_folder_tree_var,
+    "{{ folder_tree }}" // Example variable
   );
   variable_adapter_class.register(
     txt => /{{\s*folders_top\s*}}/i.test(txt),
-    replace_folders_top_var
+    replace_folders_top_var,
+    "{{ folders_top }}" // Example variable
   );
   variable_adapter_class.register(
     txt => /{{\s*(?:tags|vault_tags)\s*}}/i.test(txt),
-    replace_vault_tags_var
+    replace_vault_tags_var,
+    "{{ tags }}" // Example variable
   );
   variable_adapter_class.register(
     txt => /{{\s*recent_(\d+)\s*}}/i.test(txt),
-    replace_recent_n_var
+    replace_recent_n_var,
+    "{{ recent_n }}" // Example variable
   );
 }
