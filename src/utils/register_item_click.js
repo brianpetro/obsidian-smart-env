01ec2ff56fd2be0fdd382bcdd2697b28197a676a
Brian Joseph Petro <brian@smartconnections.app>
2025-11-17 16:54:22 -0500

    refactor: remove register_item_click utility function

--- src/utils/register_item_click.js
+++
@@ -1,9 +0,0 @@
-import { open_source } from "obsidian-smart-env/src/utils/open_source.js";
-
-export function register_item_click(container, item) {
-  container.addEventListener('click', (event) => {
-    event.stopPropagation();
-    open_source(item, event);
-    item.emit_event('connections_result:click');
-  });
-}
