4d68a45d8290d26a888278e1d63cfb0ad8d8a0d2
Brian Joseph Petro <brian@smartconnections.app>
2026-02-07 14:38:59 -0500

    feat: add smart-named-contexts icon registration

--- utils/add_icons.js
+++ utils/add_icons.js
@@ -136,6 +136,20 @@ export function add_smart_dupe_detector_icon() {
 `);
 }
 
+// register smart-named-contexts
+export function add_smart_named_contexts_icon() {
+  addIcon("smart-named-contexts", `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path>
+  <path d="M8 8h8"></path>
+  <path d="M8 11h8"></path>
+  <path d="M8 14h6"></path>
+</svg>
+`);
+}
+
+
+
 /**
  * Convenience: register all "sc-*" icons in this module.
  */
@@ -145,4 +159,5 @@ export function add_smart_icons() {
   add_inline_connections_icon();
   add_footer_connections_icon();
   add_smart_dupe_detector_icon();
+  add_smart_named_contexts_icon();
 }
