12d195b8dcbd9fbbfd69f4d2e0dd27500ed6cea3
Brian Joseph Petro <brian@smartconnections.app>
2026-03-25 14:39:22 -0400

    refactor: enhance remove_by_path logic to promote items from named contexts when removing paths

--- src/items/smart_context.js
+++ src/items/smart_context.js
@@ -1,6 +1,13 @@
 import { SmartContext as BaseClass } from 'smart-contexts/smart_context.js';
 
 export class SmartContext extends BaseClass {
+  get named_contexts () {
+    return Object.entries(this.data?.context_items || {})
+      .filter(([name, item_data]) => item_data?.named_context)
+      .map(([name, item_data]) => this.env.smart_contexts.get_named_context(name))
+      .filter(Boolean)
+    ;
+  }
   /**
    * @param {string} target_path
    */
@@ -9,16 +16,21 @@ export class SmartContext extends BaseClass {
     if (item_data) {
       delete this.data.context_items[target_path];
     } else {
-      const starts_with_items = Object.entries(this.data?.context_items || {}).filter(([key]) => key.startsWith(`${target_path}/`));
+      const starts_with_items = Object.entries(this.data?.context_items || {}).filter(([key]) => key.startsWith(target_path));
       starts_with_items.forEach(([key]) => {
         delete this.data.context_items[key];
       });
       if(!starts_with_items.length) {
-        this.data.exclusions[target_path] = {
-          key: target_path,
-          exclude: true,
-          ...(params.folder ? { folder: true } : {}),
-        };
+        // IF REMOVED FROM NAMED CONTEXT, THEN PROMOTE ALL ITEMS IN NAMED CONTEXT TO ROOT LEVEL (EXCEPT THE REMOVED ITEM)
+        const included_in_named_context = this.named_contexts.find((ctx) => Object.entries(ctx.data?.context_items || {}).some(([key]) => key.startsWith(target_path)));
+        if(included_in_named_context) {
+          delete this.data.context_items[included_in_named_context.data.name];
+          const replace_with_items = Object.entries(included_in_named_context.data?.context_items || {}).filter(([key]) => !key.startsWith(target_path));
+          replace_with_items.forEach(([key, item_data]) => {
+            this.data.context_items[key] = item_data;
+          });
+        }
+         
       }
     }
     this.emit_event('context:updated', {
